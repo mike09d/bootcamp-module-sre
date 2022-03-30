b5b46859d00a26138a9c54142b5b7df34046abb5
EC2 Default User <ec2-user@ip-172-31-7-170.ec2.internal>
2022-03-30 22:56:18 +0000

    04

--- talleres/02/terraform/remote_state.tf
+++ talleres/02/terraform/remote_state.tf
@@ -6,22 +6,3 @@ terraform {
     }
   }
 }
-terraform {
-  # backend "s3" {
-  #   bucket               = "bi-sre-terraform-state"
-  #   key                  = "terraform.tfstate"
-  #   region               = "us-east-1"
-  #   dynamodb_table       = "bi-sre-terraform-state"
-  #   workspace_key_prefix = "bi/env:"
-  #   encrypt              = true
-  # }
-}
-
-# data "terraform_remote_state" "state" {
-#   backend = "s3"
-#   config = {
-#     bucket = "bi-sre-terraform-state"
-#     key    = "bi/env:/${local.environment}/terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
---
+++ talleres/04/README.md
@@ -0,0 +1,5 @@
+# Talleres
+
+1. [Taller 1](README.DOCKER.MD): Contenerizacion
+2. [Taller 2](README.ECR.MD): Creación de recurso ECR AWS
+3. [Taller 3](README.CODEBUILD_TEST.MD): Configurando pruebas unitarias
