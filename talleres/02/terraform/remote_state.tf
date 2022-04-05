2b1dd824f5f57c91a348d1e9b90961c1c0654eb3
EC2 Default User <ec2-user@ip-172-31-7-170.ec2.internal>
2022-04-05 00:14:42 +0000

    terraform

--- talleres/02/terraform/remote_state.tf
+++ talleres/02/terraform/remote_state.tf
@@ -6,3 +6,14 @@ terraform {
     }
   }
 }
+
+terraform {
+   backend "s3" {
+     bucket               = "bs-sre"
+     key                  = "terraform.tfstate"
+     region               = "us-east-1"
+     dynamodb_table       = "bi-sre-terraform-state"
+     workspace_key_prefix = "bi/env:"
+     encrypt              = true
+   }
+}
