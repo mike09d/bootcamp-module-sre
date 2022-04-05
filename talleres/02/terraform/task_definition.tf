7985124fe5726d4f8415460f6ecb534a875bbfd2
EC2 Default User <ec2-user@ip-172-31-7-170.ec2.internal>
2022-04-05 01:58:47 +0000

    ecs

---
+++ talleres/02/terraform/task_definition.tf
@@ -0,0 +1,42 @@
+resource "aws_ecs_task_definition" "main" {
+                  family                   = var.name
+                  task_role_arn = aws_iam_role.task_role.arn
+                  execution_role_arn = aws_iam_role.main_ecs_tasks.arn
+                  network_mode             = "awsvpc"
+                  requires_compatibilities = ["FARGATE"]
+                  cpu                      = var.fargate_cpu
+                  memory                   = var.fargate_memory
+                  container_definitions = jsonencode([
+                    {
+                      name        = var.name
+                      image       = var.app_image
+                      cpu         = var.fargate_cpu
+                      memory      = var.fargate_memory
+                      networkMode = "awsvpc"
+                      ulimits = [
+                                {
+                                  "name": "nofile",
+                                  "softLimit": 65536,
+                                  "hardLimit": 65536
+                                }
+                              ]
+                      portMappings = [
+                        {
+                          containerPort = var.app_port
+                          protocol      = "tcp"
+                          hostPort      = var.app_port
+                        }
+                      ]
+                      environment = var.environment_variables
+                      logConfiguration : {
+                        logDriver : "awslogs"
+                        options : {
+                          awslogs-group = "awslogs/${var.name}/fargate"
+                          awslogs-region = "us-east-1"
+                          awslogs-stream-prefix = "streaming"
+                          awslogs-create-group = "true"
+                        }
+                      }
+                    }
+                  ])
+                }
